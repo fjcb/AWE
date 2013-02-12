69ef89b0e741c1b33862c8c6414a13c94960c0c9
fjcb <fritz@jacob-meyer.de>
2013-02-12 09:57:42 +0100

    Works better now, but some tests are still failing

--- Aufgabe2/RomanNumerals/roman_numerals.rb
+++ Aufgabe2/RomanNumerals/roman_numerals.rb
@@ -5,32 +5,55 @@ class Roman_Numerals
     input_values = {
       'ma'=>4000,
       'a'=>5000,
-      'm'=>1000,
+      '_m'=>{'mmm'=>3000, 'mm'=>2000, 'm'=>1000},
       'cm'=>900,
       'cd'=>400,
       'xc'=>90,
       'd'=>500,
-      'c'=>100,
+      '_c'=>{'ccc'=>300, 'cc'=>200, 'c'=>100},
       'xl'=>40,
       'l'=>50,
       'ix'=>9,
-      'x'=>10,
+      '_x'=>{'xxx'=>30, 'xx'=>20, 'x'=>10},
       'iv'=>4,
       'v'=>5,
-      'i'=>1
+      '_i'=>{'iii'=>3, 'ii'=>2, 'i'=>1}
     } 
     
     input_string = input_string.downcase
 	  output = 0
 	
-	  while  input_string != ""
-			input_values.each { |i, v|
+		input_values.each { |i, v|
+		  
+		  #just take one element of i, x, c, m
+		  if v.class == Hash
+		    max = 0
+		    v.each { |ii, vv|
+		      if input_string.include? ii
+		        max = [max,vv].max
+		      end 
+		    }
+		    
+		    v.each { |ii, vv|
+		      if vv == max
+		        input_string.slice!(ii)
+		        output += vv
+		      end  
+		    }
+		    
+		  end
+		  
+		  #check the rest
       if input_string.slice!(i) != nil
-        output += v
+        output += v  
       end
     }
-		end
 
-	  return output
+    #if the given string is not empty now, something was wrong with it
+    if !input_string.empty?
+      return 0
+    else
+	    return output
+	  end
   end  
 end
--- Aufgabe2/RomanNumerals/tc_roman_numerals.rb
+++ Aufgabe2/RomanNumerals/tc_roman_numerals.rb
@@ -9,6 +9,7 @@ class Test_Roman_Numerals < Test::Unit::TestCase
     assert_equal(1, Roman_Numerals.parse("i"))
 	  assert_equal(1, Roman_Numerals.parse("I"))
 	  assert_equal(5, Roman_Numerals.parse("V"))
+	  assert_equal(0, Roman_Numerals.parse("xyz"))
 	  assert_equal(8, Roman_Numerals.parse("viii"))
 	  assert_equal(4, Roman_Numerals.parse("iv"))
 	  assert_equal(13, Roman_Numerals.parse("xiii"))
@@ -17,12 +18,9 @@ class Test_Roman_Numerals < Test::Unit::TestCase
 	  assert_equal(444, Roman_Numerals.parse("cdxliv"))
 	  assert_equal(2013, Roman_Numerals.parse("mmxiii"))
 	  assert_not_equal(4, Roman_Numerals.parse("iiii"))
-  end
-  
-  def test_typecheck
-    assert_raise(RuntimeError) { Roman_Numerals.parse("iiv") }
-	  assert_raise(RuntimeError) { Roman_Numerals.parse("vic") }
-	  assert_raise(RuntimeError) { Roman_Numerals.parse("mmcccciiiv") }
+	  assert_not_equal(5, Roman_Numerals.parse("iiv"))
+	  assert_not_equal(16, Roman_Numerals.parse("vic"))
+	  assert_not_equal(2406, Roman_Numerals.parse("mmcccciiiv"))
   end
 
 end
